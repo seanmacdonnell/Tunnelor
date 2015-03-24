4e96a23a913c2e82b8e2226acc84654b20b7df44
Sean MacDonnell <sean.macdonnell@gmail.com>
2015-03-24 19:01:26 +0800

    Fixed a bug with the procedural level generation where left and right walls were not being correctly detected as walls in the avatar

--- Tunnelour/Tunnelour/include/Colour_Helper.h
+++ Tunnelour/Tunnelour/include/Colour_Helper.h
@@ -40,19 +40,18 @@ namespace Tunnelour {
                                                  0.788235294117647f,
                                                  1.0f);
                                                  */
-     const D3DXCOLOR Text_Blue = D3DXCOLOR(0.2607843137254902f,
-                                           0.3705882352941176f,
-                                           0.5862745098039216f,
-                                           1.0f);
-
      const D3DXCOLOR Text_Red = D3DXCOLOR(0.7529411764705882f,
                                           0.3f,
                                           0.0f,
                                           1.0f);
 
-     const D3DXCOLOR Text_Light_Blue = D3DXCOLOR(0.2392156862745098f,
-                                                 0.4f,
-                                                 0.7215686274509804f,
+     const D3DXCOLOR Text_Light_Blue = D3DXCOLOR(0.2941176470588235f,
+                                                 0.4549019607843137f,
+                                                 0.7843137254901961F,
+                                                 1.0f);
+     const D3DXCOLOR Text_Blue = D3DXCOLOR(0.2941176470588235f,
+                                                 0.4549019607843137f,
+                                                 0.7843137254901961F,
                                                  1.0f);
  
   }  // namespace Colours
